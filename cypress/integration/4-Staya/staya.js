a8685479c6a03bfb7d1a245d5008ec28b1d57310
Лукьянченко Данила <116444575+danilalukyanchenkoQA@users.noreply.github.com>
2022-11-27 20:28:14 +0300

    Add files via upload

---
+++ cypress/integration/4-Staya/staya.js
@@ -0,0 +1,18 @@
+
+describe('Проверка формы логина Staya', function () {
+
+   it('Открываю страницу входа/регистрации', function () {
+   cy.visit('https://staya.dog/');
+   cy.get('.BF2022Modal__actions > .s-button > .s-button__content').click();
+   cy.wait(2000);
+   cy.get('.header-bottom__right--link').click();
+   cy.get('.auth-form > form > [type="email"]').type('danila.kaluga26@gmail.com');
+   cy.get('.auth-form > form > [type="password"]').type('Luchok228');
+   cy.get('.auth-form__submit > .s-button__content').click();
+   cy.wait(2000);
+   cy.contains('Мои заказы');
+    })
+
+
+})
+
