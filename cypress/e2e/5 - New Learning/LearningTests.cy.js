87fba3b128f56b09040cc0ceed427c396a3b53bd
dlukyanchenko <dlukyanchenko@Tehnosk.ru>
2024-02-15 14:01:55 +0300

    Refactoring case

--- cypress/e2e/5 - New Learning/LearningTests.cy.js
+++ cypress/e2e/5 - New Learning/LearningTests.cy.js
@@ -1,21 +1,22 @@
 describe("Авторизация", function() {
 
 
-     it("Вход с валидным логином и паролем", function() {
+     it.only("Вход с валидным логином и паролем", function() {
         cy.visit("https://unsplash.com/");
-        cy.contains("Log in").click();
-        cy.contains("Email" );
+        cy.title().should('include', 'Unsplash');
+        cy.contains("Log in").should('be.visible').click();
+        cy.contains("Email" );  
         cy.get(':nth-child(1) > .ksgc2').type("danila.kaluga26@gmail.com");
         cy.get(':nth-child(2) > .ksgc2').type("Luchok228");
         cy.get('.a1ISs').click();
-        cy.get('.ptze8').click();
+        cy.get('.ptze8').should('be.visible').click();
         cy.get(':nth-child(3) > li > .KR60y').should('have.text', 'Logout @danilaqa');
-        cy.get(':nth-child(3) > li > .KR60y');
      });
      
      it("Выход из системы", function() {
         cy.visit("https://unsplash.com/");
-        cy.contains("Log in").click();
+        cy.title().should('include', 'Unsplash');
+        cy.contains("Log in").should('be.visible').click();
         cy.contains("Email" );
         cy.get(':nth-child(1) > .ksgc2').type("danila.kaluga26@gmail.com");
         cy.get(':nth-child(2) > .ksgc2').type("Luchok228");
