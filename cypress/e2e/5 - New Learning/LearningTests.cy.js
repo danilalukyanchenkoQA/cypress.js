fed4b0393d6df64d69a30920ba9a68ae4f8f5c69
danilalukyanchenkoQA <danila.kaluga26@gmail.com>
2024-09-25 18:58:40 +0300

    Fixed selectors

--- cypress/e2e/5 - New Learning/LearningTests.cy.js
+++ cypress/e2e/5 - New Learning/LearningTests.cy.js
@@ -21,20 +21,21 @@ const PASSWORD = 'Luchok228'
         cy.visit("https://unsplash.com/login");
         cy.url().should('include', 'https://unsplash.com/login'); 
         cy.get('input[name="email"]').type(USER_EMAIL);
-        cy.get('input[name="password"]').type(PASSWORD);
-        cy.get('button[class="IUoDf Qzy6N ogSND ZR5jm cs1e4 ibgtd LoCAy mP89P szXtT DA6Wz"]').click(); 
+        cy.get('input[name="password"]').type(PASSWORD);   
+        cy.get('button[value="Login"]').click(); 
         cy.get('.wcgxB').should('not.exist');
      });
      
      it("Выход из системы", function() {
         cy.visit("https://unsplash.com/login");
         cy.url().should('include', 'https://unsplash.com/login'); 
-        cy.get(':nth-child(1) > .ffHip').type(USER_EMAIL); 
-        cy.get(':nth-child(2) > .ffHip').type(PASSWORD);
-        cy.get('button[class="IUoDf Qzy6N ogSND ZR5jm cs1e4 ibgtd LoCAy mP89P szXtT DA6Wz"]').click(); 
+        cy.get('input[name="email"]').type(USER_EMAIL); 
+        cy.get('input[name="password"]').type(PASSWORD); 
+        cy.get('button[value="Login"]').click();
         cy.get('.wcgxB').should('not.exist');
-        cy.get('img[class="Lg6wf DlmAi"]').click();
-        cy.get('button[class="wH4EU FEdrY zZzwR OvNxk VHcyz cs1e4 f62h9 thIqV"]').click();  
+        cy.get('img[class="voBgy SycQK"]').click();
+        cy.wait(4000)
+        cy.contains('Logout @danilaqa').click();  
         cy.contains("Log in");
         cy.get('img[class="Lg6wf DlmAi"]').should('not.exist');
     });      
@@ -44,7 +45,7 @@ const PASSWORD = 'Luchok228'
         cy.url().should('include', 'https://unsplash.com/login'); 
         cy.get('input[name="email"]').type("danila.kaluga2546@gmail.com");
         cy.get('input[name="password"]').type('Luchok22348{enter}');     
-        cy.get('div[class="xLxmF hnFOY"]').contains("Invalid email or password.");
+        cy.contains("Invalid email or password.");
     });
 });
 
@@ -52,21 +53,21 @@ describe("Основные функции сайта", function() {
 
     it("Строка поиска(верхняя)+Url", function() {
         cy.visit("https://unsplash.com/");
-        cy.get('[data-test="nav-bar-search-form-input"]').type('summer{enter}');
+        cy.get('input[class="gKBw1 uq8A1"]').type('summer{enter}');
         cy.title().should('include', 'Summer');
         cy.url().should('include', 'https://unsplash.com/s/photos/summer');
     });
 
      it("Строка поиска(нижняя)+Url", function() {
         cy.visit("https://unsplash.com/");
-        cy.get('[data-test="homepage-header-search-form-input"]').type('winter{enter}');
+        cy.get('input[class="rAfGd uq8A1"]').type('winter{enter}');
         cy.title().should('include', 'Winter');
         cy.url().should('include', 'https://unsplash.com/s/photos/winter');
     });
 
     it("Поиск+Скролл", function() {
         cy.visit("https://unsplash.com/");
-        cy.get('[data-test="homepage-header-search-form-input"]').type('car{enter}');
+        cy.get('input[class="rAfGd uq8A1"]').type('car{enter}');
         cy.title().should('include', 'Car');
         cy.url().should('include', 'https://unsplash.com/s/photos/car');
         cy.scrollTo('bottom');
@@ -78,8 +79,8 @@ describe("Основные функции сайта", function() {
 
     it("Поиск без результата(Ничего не найдено)", function() {
         cy.visit("https://unsplash.com/");
-        cy.get('[data-test="homepage-header-search-form-input"]').type('dfgshghsdldkjs{enter}');
-        cy.get('img[class="AAazD"]').should('be.visible');
+        cy.get('input[class="rAfGd uq8A1"]').type('dfgshghsdldkjs{enter}');
+        cy.get('img[class="ausjZ"]').should('be.visible');
         cy.contains("Photos 0");
         cy.contains("Collections 0");
     });
